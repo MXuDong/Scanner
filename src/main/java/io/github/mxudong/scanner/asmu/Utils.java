5e686286aa67951304d5dc73a6cdcba6552b4882
MXuDong <2941884109@qq.com>
2019-11-08 18:31:53 +0800

    add plugins

---
+++ src/main/java/io/github/mxudong/scanner/asmu/Utils.java
@@ -0,0 +1,91 @@
+package io.github.mxudong.scanner.asmu;
+
+import io.github.mxudong.scanner.Path;
+
+import java.io.*;
+
+/**
+ * @author Dong
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
+public class Utils {
+
+    /**
+     * packing the file input stream
+     */
+    public class FileInputStreamUtil {
+        private File file;
+        private FileInputStream fileInputStream;
+
+        /**
+         * The construction, received type : file
+         *
+         * @param file target file
+         */
+        public FileInputStreamUtil(File file) {
+            if (file.exists() && file.isFile()) {
+                this.file = file;
+                openInputStream();
+            } else {
+                this.file = null;
+                this.fileInputStream = null;
+            }
+        }
+
+        /**
+         * The construction, receive type : path
+         *
+         * @param path target file path
+         */
+        public FileInputStreamUtil(Path path) {
+            this(path.generatorFile());
+        }
+
+        /**
+         * close inner file input stream, if is closed, will do nothing
+         */
+        public void closeInputStream() {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException ignore) {
+                } finally {
+                    fileInputStream = null;
+                }
+            }
+        }
+
+        /**
+         * get inner file input stream, if not exits, it will open stream
+         *
+         * @return input file stream
+         */
+        public InputStream getInputStream() {
+            if(fileInputStream == null){
+                openInputStream();
+            }
+
+            return fileInputStream;
+        }
+
+        /**
+         * open file input stream, if is opened, will close it first.
+         *
+         * @return input stream
+         */
+        public InputStream openInputStream() {
+            closeInputStream();
+
+            try {
+                this.fileInputStream = new FileInputStream(file);
+            } catch (FileNotFoundException ignore) {
+                this.fileInputStream = null;
+            }
+
+            return fileInputStream;
+        }
+
+    }
+}
---
+++ src/main/java/io/github/mxudong/scanner/asmu/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * This package provide some method to solve ASM framework,
+ * such as check annotation in class file. For scanner rules,
+ * this package will provide many method to search, delete
+ * and update class file.
+ * <p>
+ * And in the other hand, this package also packing the CGLib
+ * method for use quickly, all the class file will be convert
+ * to the {@code Path(io.github.mxudong.scanner.Path)}.
+ * <p>
+ * The utils packing the common utils.
+ * <ol>
+ * <li>1. FileInputStreamUtils packing the file input stream operation.</li>
+ * </ol>
+ */
+package io.github.mxudong.scanner.asmu;
